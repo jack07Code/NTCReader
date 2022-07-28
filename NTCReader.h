15e6a7c9c96a28c41ac014343cb0953558a68618
jack07Code <giacomomrtnll@gmail.com>
2022-07-28 17:55:39 +0200

    Aggiunta formula

--- NTCReader.h
+++ NTCReader.h
@@ -19,6 +19,11 @@ class NTCReader {
 		double A=1.189148e-3;
 		double B=2.34125e-4;
 		double C=8.76741e-8;
+		double SteinhartHart(double R) {
+			double logR=log(R);
+			double logR3=logR*logR*logR;
+			return 1.0/(A+B*logR+C*logR3);
+		};
 };
 
 #endif
